0477e675230c44ea877466b721f5a2b9432110a5
Janet Leon <ajleon00@gmail.com>
2019-03-31 22:52:05 -0700

    sets up sockets.io

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+/* eslint-disable indent */
+/* eslint-disable no-console */
 'use strict';
 const express = require('express');
+const socket = require('socket.io');
 //APP setup
 const app = express();
 const server = app.listen(8080, function() {
-	console.log('Listening on 8080');
+	console.log('server running on 8080');
 });
 
 //Static Files
 app.use(express.static('public'));
+
+//Socket Setup
+const io = socket(server);
+
+io.on('connection', function() {
+	console.log('made socket connection');
+});
